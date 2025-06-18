54513976376f89e218e6e6e8b6d4d8d1feaae871
Felipe Colli <felipe.colli.scratch@gmail.com>
2025-06-18 17:05:26 -0400

    Auto-commit: 2025-06-18 17:05:25

--- C++/prog_comp/Probs/usaco/speeding/speeding.cpp
+++ C++/prog_comp/Probs/usaco/speeding/speeding.cpp
@@ -1,10 +1,47 @@
 #include <bits/stdc++.h>
+#include <climits>
+#include <cstdio>
+#include <iostream>
 using namespace std;
 #define ll long long
 #define str string
 #define vec vector
 
 int main() {
+    vec<int> road(100);
+    vec<int> trave(100);
+    freopen("speeding.in", "r", stdin);
+    freopen("speeding.out", "w", stdout);
+
+    int n, m;
+    int maxi = 0;
+
+    cin >> n >> m;
+    int cont = 0;
     
+    int tam, vel;
+    for (int i = 0; i < n; i++) {
+        cin >> tam >> vel;
+        for (int j = 0; j < tam; j++) {
+            road[cont+j]=vel;
+        }
+        cont+=tam;
+    }
+    cont = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> tam >> vel;
+        for (int j = 0; j < tam; j++) {
+            trave[cont+j]=vel;
+        }
+        cont+=tam;
+    }
+
+    for (int i = 0; i < 100; i++) {
+        if (trave[i]>road[i]) {
+            maxi = max(maxi, (trave[i]-road[i]));
+        }
+    }
+    //printf("%d\n", maxi);
+    cout << maxi << "\n";
     return 0;
 }
---
+++ C++/prog_comp/Probs/usaco/traffic/traffic.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+#define vec vector
+
+int main() {
+    
+    return 0;
+}
