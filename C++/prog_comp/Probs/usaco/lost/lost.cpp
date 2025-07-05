7a288e86341bd3ae2d403b9fac7c51c774199c14
Felipe Colli <felipe.colli.scratch@gmail.com>
2025-07-05 12:15:13 -0400

    Respaldando

---
+++ C++/prog_comp/Probs/usaco/lost/lost.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define str string
+#define vec vector
+
+int main() {
+    int ini, vaca;
+    cin >> ini >> vaca;
+
+    int pos,ubi, suma;
+    suma = 0;
+    pos = ini;
+    int i= 1;
+    if (pos)
+
+    while (pos != vaca) {
+        pos += i;
+        if (pos==vaca) {
+
+        }
+    }
+
+    return 0;
+}
