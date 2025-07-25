db23181f9aa383253052c267f9e5b99260648793
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-07-25 17:07:39 -0400

    Auto-commit: 2025-07-25 17:07:37

---
+++ C++/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef string str;
+#define vec vector
+
+int main() {
+    cout << "Mundo Hola" << endl;
+    return 0;
+}
