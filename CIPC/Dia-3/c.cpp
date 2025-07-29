1c06b8cea37726585c6bc8645b7b1c2fcc6955d3
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-07-29 09:29:42 -0400

    Patata: 2025-07-29 09:29:40

--- CIPC/Dia-3/c.cpp
+++ CIPC/Dia-3/c.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <deque>
 using namespace std;
 
 typedef long long ll;
@@ -6,7 +7,32 @@ typedef string str;
 #define vec vector
 
 int main() {
+    int n, k;
+    cin >> n >> k;
     
+    vec <int> id(n);
+    for (int i = 0; i < n; i++) cin >> id[i];
+    
+    deque <int> pant;
+    set <int> in;
+
+    for (int i = 0; i < n; i++) {
+        if (in.find(id[i]) == in.end()) {
+            if (in.size() == k) {
+                int del = pant.back();
+                pant.pop_back();
+                in.erase(del);
+            }
+            pant.push_front(id[i]);
+            in.insert(id[i]);
+        }
+    }
+    cout << in.size() << "\n";
+    while (!pant.empty()) {
+        cout << pant.front() << " ";
+        pant.pop_front();
+    }
+
     return 0;
 }
 
