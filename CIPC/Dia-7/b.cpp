e0f68ed83ae03c039eda7dee38cde994c53078f6
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-07-29 23:32:47 -0400

    Patata: 2025-07-29 23:32:45

--- CIPC/Dia-7/b.cpp
+++ CIPC/Dia-7/b.cpp
@@ -2,11 +2,30 @@
 #include <cmath>
 using namespace std;
 
+const int MAXA = 10000001;
+int primo_chico[MAXA];
+
+
 typedef long long ll;
 typedef string str;
 #define vec vector
 
+void sieve () {
+    iota(primo_chico, primo_chico + MAXA, 0);
+    for (int i = 2; i*i < MAXA; i++) {
+        if (primo_chico[i] == i) {
+            for (int j = i * i; j < MAXA; j += i) {
+                if (primo_chico[j] == j) primo_chico[j] = i;
+            }
+        }
+    }
+}
+
+
+
 int main() {
+    sieve();
+
     int n;
     cin >> n;
     vec <int> a(n);
@@ -14,31 +33,24 @@ int main() {
     
     vec <int> d1 (n, -1), d2(n, -1);
 
-    vec <vec <int>> divisores(n);
-    for (int j  = 0; j < n; j++) {
-        for (int i = 1; i <= (int)sqrt(a[j]) + 1; i++) {
-            if (a[j] % i == 0 && i * i == a[j]) divisores[j].push_back(i);
-            else if (a[j] % i == 0 && i * i != a[j] && i > 1 && a[j] / i > 1) {
-                divisores[j].push_back(i);
-                divisores[j].push_back(a[j] / i);
-            }
-        }
-    }
-    bool temp = 0;
     for (int i = 0; i < n; i++) {
-        temp = 0;
-        for (int j = 0; j < divisores[i].size(); j++) {
-            for (int k = j + 1; k < divisores[i].size(); k++) {
-               if (gcd(divisores[i][j] + divisores[i][k], a[i]) == 1) {
-                    d1[i] = divisores[i][j];
-                    d2[i] = divisores[i][k];
-                    temp = 1;
-                    break;
-                } 
-            }
-            if (temp) break;
+        int act = a[i];
+        int numi = primo_chico[act];
+
+        if (numi == act) continue;
+
+        int ff = 1;
+        while (act % numi == 0) {
+            ff *= numi;
+            act /= numi;
+        }
+        if (act > 1) {
+            d1[i] = ff;
+            d2[i] = act;
         }
     }
+
+
 
     for (int i = 0; i < n; i++) cout << d1[i] << " ";
     cout << endl;
