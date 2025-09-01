807012d9fd227e693c5294658537c46a807d8c54
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-09-01 16:33:34 -0400

    Auto-commit: 2025-09-01 16:33:33

--- C++/prog_comp/Probs/cses/introductory/spiral.cpp
+++ C++/prog_comp/Probs/cses/introductory/spiral.cpp
@@ -6,6 +6,25 @@ typedef string str;
 #define vec vector
 
 int main() {
-    
+    int t;
+    cin >> t;
+
+    int x,y;
+    while (t--) {
+        cin >> y >> x;
+
+        ll m = max(x,y);
+        ll ant = (m-1)*(m-1);
+
+        if (m % 2 == 1) {
+            if (x == m) cout << m * m - (y - 1) << endl;
+            else cout << ant + x << endl;
+        }
+        else {
+            if (y == m) cout << m * m - (x - 1) << endl;
+            else cout << ant + y << endl;
+        }
+        
+    }
     return 0;
 }
