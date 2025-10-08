3a2f0e6d17735ce5ce104a8f22b66d50b5b23304
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-10-07 22:10:49 -0300

    SAQUÉ EL K, VAMOS CTM

--- Competitive/TCP/k.cpp
+++ Competitive/TCP/k.cpp
@@ -1,21 +1,42 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-const long long MOD = 1000000000 + 7;
+typedef long long ll;
+typedef string str;
+#define vec vector
+
+const ll MOD = 1e9 +7;
+
+vec <ll> mem;
+vec <ll> fac;
+
+ll dp (int n) {
+    
+    if (n == 0) return 1;
+    if (n == 1) return 2;
+    if (mem[n] == -1) {
+        mem[n] = (fac[n] + 2 * dp(n - 1)) % MOD;
+    }
+
+    return mem[n] % MOD;
+}
+
 
 int main() {
     int n;
     cin >> n;
 
-    long long out = 1;
-    int z = n;
-
-    while (z > 1) {
-        if (z == n) out *= 3;
-        else out *= z;
-        z--;
-        out = out % MOD;
+    mem.resize(n, -1);
+    fac.resize(n, -1);
+    
+    fac[0] = 1;
+    for (int i = 1; i < n; i++) {
+        fac[i] = (fac[i - 1] * i) % MOD;
     }
-    cout << out << endl;
+    
+
+
+    cout << dp(n - 1) << endl;
+
+    return 0;
 }
