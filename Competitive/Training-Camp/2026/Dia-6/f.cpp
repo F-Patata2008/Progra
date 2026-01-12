669e7a26445916bf01093bcf5e1dec32457ec526
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-01-12 17:58:25 -0300

    chore: Uploading Prog comp

--- Competitive/Training-Camp/2026/Dia-6/f.cpp
+++ Competitive/Training-Camp/2026/Dia-6/f.cpp
@@ -1,38 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef string str;
-#define vec vector
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
 
-int n;
-vec<int> a;
+    // Probamos todas las máscaras desde 0 hasta 2^n - 1
+    // (1 << n) es lo mismo que 2 elevado a n
+    for (int mask = 0; mask < (1 << n); mask++) {
+        
+        string p = "";      // Subsecuencia a extraer
+        string x = "";      // Lo que queda
+        vector<int> indices_p; // Para guardar los índices (1-based)
 
-int eli () {
-    for (int  i = 0; i < pow(2, n); i++) {
+        for (int i = 0; i < n; i++) {
+            // Revisamos si el bit i está encendido en la máscara
+            // (mask >> i) & 1  verifica el i-ésimo bit
+            if ((mask >> i) & 1) {
+                p += s[i];
+                indices_p.push_back(i + 1); // Guardamos índice + 1
+            } else {
+                x += s[i];
+            }
+        }
 
+        // Condición 1: p debe ser no-decreciente (sorted)
+        // Como es binario, significa que no puede haber un '0' después de un '1'
+        bool p_sorted = true;
+        for (int i = 0; i + 1 < p.size(); i++) {
+            if (p[i] > p[i+1]) {
+                p_sorted = false;
+                break;
+            }
+        }
+        // Alternativa moderna: is_sorted(p.begin(), p.end())
+
+        // Condición 2: x debe ser palíndromo
+        bool x_palindrome = true;
+        for (int i = 0; i < x.size() / 2; i++) {
+            if (x[i] != x[x.size() - 1 - i]) {
+                x_palindrome = false;
+                break;
+            }
+        }
+
+        // Si cumple ambas condiciones, imprimimos y terminamos este caso
+        if (p_sorted && x_palindrome) {
+            cout << indices_p.size() << "\n";
+            for (int i = 0; i < indices_p.size(); i++) {
+                cout << indices_p[i] << (i == indices_p.size() - 1 ? "" : " ");
+            }
+            cout << "\n";
+            return; // Encontramos una solución, salimos de la función solve()
+        }
     }
 
-
-    bool pal = 1;
-    for (int i = 0; i < a.size() / 2; i++) {
-        if (a[i] != a[n - i - 1]) pal = 0;
-    }
-    return 0;
+    // Si probamos todo y nada funcionó (aunque el problema garantiza solución o -1)
+    cout << -1 << "\n";
 }
 
-
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin >> t;
     while (t--) {
-        cin >> n;
-        a.assign(n, -1);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-
-
-
+        solve();
     }
     return 0;
 }
--- Competitive/Training-Camp/2026/Dia-6/j.cpp
+++ Competitive/Training-Camp/2026/Dia-6/j.cpp
@@ -2,22 +2,53 @@
 using namespace std;
 
 typedef long long ll;
-typedef string str;
-#define vec vector
 
-int l, r;
+void solve() {
+    int n;
+    cin >> n;
 
-void nums() {
+    cout << "1 1 " << n << endl; // Query tipo 1: suma original
+    ll sum_p_total;
+    cin >> sum_p_total;
 
+    cout << "2 1 " << n << endl; // Query tipo 2: suma modificada
+    ll sum_a_total;
+    cin >> sum_a_total;
+
+    int length = sum_a_total - sum_p_total;
+
+    int low = 1, high = n;
+    int l = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        cout << "1 1 " << mid << endl;
+        ll s1;
+        cin >> s1;
+
+        cout << "2 1 " << mid << endl;
+        ll s2;
+        cin >> s2;
+
+        if (s1 != s2) {
+            l = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    int r = l + length - 1;
+
+    cout << "! " << l << " " << r << endl;
 }
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        
+        solve();
     }
     return 0;
 }
