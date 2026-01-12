a42a448dfdbeda2e51c2df2ba01f511ddaab4f55
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-01-12 18:04:11 -0300

    Auto-commit: 2026-01-12 18:04:09

--- Competitive/Training-Camp/2026/Dia-6/a.cpp
+++ Competitive/Training-Camp/2026/Dia-6/a.cpp
@@ -17,15 +17,12 @@ int main() {
         for (int i = 0; i < n; i++) cin >> a[i];
 
         int maxi = INT_MIN;
-        int r, l;
         for (int i = 0; i < n; i++) {
             int sum = 0;
             for (int j = i; j < n; j++) {
                 sum += a[j];
                 if (sum > maxi * (j - i + 1)) {
                     maxi = sum / (j - i + 1);
-                    r = j;
-                    l = i;
                 }
             }
         }
