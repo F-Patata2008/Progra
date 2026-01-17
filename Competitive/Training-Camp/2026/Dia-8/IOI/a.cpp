9c91a2c0452f8e3e2ffa28f95dd3efae3d839988
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-01-16 21:46:00 -0300

    Sauqe el puto probelma

--- Competitive/Training-Camp/2026/Dia-8/IOI/a.cpp
+++ Competitive/Training-Camp/2026/Dia-8/IOI/a.cpp
@@ -24,4 +24,31 @@ int mote_con_huesito (int n) {
     }
 
     return hueso ;
+#include <bits/stdc++.h>
+#include "mote.h"
+using namespace std;
+
+typedef long long ll;
+typedef string str;
+#define vec vector
+
+
+int mote_con_huesito (int n) {
+    vec<vec<int>> probs;
+    for (int i = 0; (1<<i) <= n; i++) {
+        vec <int> patata;
+        for (int j = 1; j <= n; j++) {
+            if (j & (1<<i)) patata.push_back(j);
+        }
+        probs.push_back(patata);
+    }
+    vec<bool> pos = laboratorio(probs);
+
+    int hueso = 0;
+    for (int i = 0; i < pos.size(); i++) {
+        if (pos[i]) hueso |= (1<<i);
+    }
+
+    return hueso ;
+}
 }
