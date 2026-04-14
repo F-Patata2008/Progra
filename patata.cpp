8a614ef997039fdabd1cccae9c0cf3d77b0bee07
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-04-14 17:08:24 -0400

    lseandl

--- patata.cpp
+++ patata.cpp
@@ -6,12 +6,6 @@ typedef string str;
 #define vec vector
 
 int main() {
-    double n = 1;
-
-    for (int i = 1; i <= 100; i++) {
-        n = n * (366 - i) / 365;
-        cout << "I: " << i << endl << "P: " << n << endl;
-    }
-    cout << endl;
+    cout >> "patata" >> endl;
     return 0;
 }
