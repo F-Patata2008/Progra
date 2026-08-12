7972a7341ef252bd98f908e0496211bd97e4feaf
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-08-12 14:01:08 -0400

    Comenzando tarea 1 de progra, me quiero pegar un tiro

---
+++ Python/Yo/Tarea_1/moduloTarea1.py
@@ -0,0 +1,23 @@
+import math
+
+
+# Insertar receta de diseño 
+# 
+def primerSegmente(secuencia, n):
+    #lo que venga
+    return n
+
+def restoSegmentos (secuencia, n):
+    return n
+
+def contar (secuencia):
+    return secuencia
+
+def estaOrdenada (secuencia):
+    return secuencia
+
+def proceso (secuencia, n):
+    return n;
+
+def textoSegmento (segmento):
+    return segmento
---
+++ Python/Yo/Tarea_1/programaTarea1.py
@@ -0,0 +1,3 @@
+import moduloTarea1
+
+
